9efbfe2b594ad4f96c2c718c46c245564bf424a8
Linnea Sandberg <llinnea.sandberg@gmail.com>
2023-12-04 19:50:29 +0100

    now i can render a randomized image and always show the name of that student + tre other names, super hardcoded

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ const firstPageEl = document.getElementById("firstPage");
 const secondPageEl = document.getElementById("secondPage");
 const theGameEl = document.querySelector("#theGame");
 const randomNameEl = document.querySelectorAll(".randomName");
+const imageEL = document.querySelector("#studentImage");
+const choiceEl = document.querySelectorAll(".choice");
+const firstButton = document.querySelector("#firstButton");
+const secondButton = document.querySelector("#secondButton");
+const thirdButton = document.querySelector("#thirdButton");
+const forthButton = document.querySelector("#forthButton");
 
 // dom referenser till antalet rundor spelaren vill spela
 const buttonForTenEl = document.querySelector("#buttonForTen");
@@ -26,40 +32,85 @@ shuffleArray(shuffledStudents); // shuffle the `shuffledStudents` array
 console.log("Students after proper shuffling:", shuffledStudents);
 
 //här får jag ut alla namnen
-shuffledStudents.forEach((student) => {
+const studentNames = shuffledStudents.forEach((student) => {
   console.log(student.name);
+  return student.name;
 });
 
 //här får jag ut alla bilder
-shuffledStudents.forEach((student) => {
+const studentImages = shuffledStudents.forEach((student) => {
   console.log(student.image);
+  return student.image;
 });
 
-let gameStarted = false;
-let peopleToGuess = 0;
+let currentStudent;
 
-function startGame() {
-  if (gameStarted) {
-    return;
-  }
+function showImage() {
+  currentStudent = shuffledStudents[0];
+
+  imageEL.src = currentStudent.image;
+
+  console.log("hej hopp");
 }
+
+function names() {
+  let studentName = shuffledStudents[0].name;
+
+  firstButton.innerHTML = studentName;
+
+  let studentsNames = shuffledStudents[1].name;
+  secondButton.innerHTML = studentsNames;
+  thirdButton.innerHTML = studentsNames;
+  forthButton.innerHTML = studentsNames;
+}
+
+showImage();
+names();
+
+const checkAnswear = (imageName, buttonName) => {
+  if (imageName === buttonName) {
+    alert("Thats the right answear!");
+  } else {
+    alert("wrong answear!");
+  }
+};
+
+// function getRandom() {
+//   choice.textContent = students[studentNames].name;
+
+//   image.src = students[shuffledStudents].image;
+// }
 
 buttonForTenEl.addEventListener("click", (e) => {
   e.preventDefault();
-  startGame(10);
-  gameStarted = true;
+
+  showImage();
+  firstPageEl.style.display = "none";
+  secondPageEl.classList.remove("hide");
 });
 
 buttonForTwentyEl.addEventListener("click", (e) => {
   e.preventDefault();
-  startGame(20);
-  gameStarted = true;
+
+  //gameStarted = true;
+  secondPageEl.classList.remove("hide");
 });
 
 buttonForAllEl.addEventListener("click", (e) => {
   e.preventDefault();
-  startGame(41);
-  gameStarted = true;
+
+  //gameStarted = true;
+  secondPageEl.classList.remove("hide");
+});
+
+choiceEl.forEach(function (button) {
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    let image = shuffledStudents[i].image;
+    let name = shuffledStudents[i].name;
+    checkAnswear(button.name, name);
+    console.log("Här funkar det");
+  });
 });
 
 /*
@@ -85,4 +136,11 @@ myButtonsEl.forEach((button) => {
   });
 });
 
+function startGame() {
+  if (gameStarted) {
+    return;
+  }
+}
+let gameStarted = false;
+let peopleToGuess = 0;
 */
