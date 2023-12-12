3109d14261bb881474c51e50dff59ccd900f3c64
Linnea Sandberg <llinnea.sandberg@gmail.com>
2023-12-12 17:56:53 +0100

    cleaned and structured all files and added all styling

--- main.js
+++ main.js
@@ -1,26 +1,21 @@
-const firstPageEl = document.querySelector(".firstPage"); //hiding
-const howManyEl = document.querySelector("#howMany"); //also for hiding
-const secondPageEl = document.getElementById("secondPage"); //aswell for hiding
-const imageEL = document.querySelector("#studentImage"); //rendering the image
-const guessesEl = document.querySelector("#guesses"); //rendering how many guesses made
-const allGuessingButtonsEl = document.querySelector("#allGuessingButtons"); //all the buttons for guessing who's the student
-const optionsButtonsEl = document.querySelector(".optionsButtons"); //all buttons for choosing how many rounds to play
-const rightGuessesEl = document.querySelector("#rightGuesses"); //rendering how many right-guesses the user have
-const wrongGuessesEl = document.querySelector("#wrongGuesses"); //rendering how many wrong-guesses the user have
-const resultsEl = document.querySelector("#results"); //empty conatiner to display the result after the game
+const firstPageEl = document.querySelector(".firstPage"); // for display purpose
+const secondPageEl = document.getElementById("secondPage"); // for display purpose
+const imageEL = document.querySelector("#studentImage"); // rendering the image
+const guessesEl = document.querySelector("#guesses"); // displaying how many guesses made
+const allGuessingButtonsEl = document.querySelector("#allGuessingButtons"); // buttons for guessing who's the student
+const optionsButtonsEl = document.querySelector(".optionsButtons"); // buttons for choosing how many rounds to play
+const rightGuessesEl = document.querySelector("#rightGuesses"); // displaying how many right-guesses the user have
+const wrongGuessesEl = document.querySelector("#wrongGuesses"); // displaying how many wrong-guesses the user have
+const resultsEl = document.querySelector("#results"); // empty container for rendering the result
 
-//buttons for rendering the names off students
+// buttons for rendering the student names
 const firstButtonEl = document.querySelector("#firstButton");
 const secondButtonEl = document.querySelector("#secondButton");
 const thirdButtonEl = document.querySelector("#thirdButton");
 const forthButtonEl = document.querySelector("#forthButton");
 
-//each button for choosing how many rounds
-const buttonForTenEl = document.querySelector("#buttonForTen");
-const buttonForTwentyEl = document.querySelector("#buttonForTwenty");
-const buttonForAllEl = document.querySelector("#buttonForAll");
-
 //variables for the global-scoope
+let currentIndex = 0; // index for randomize current student
 let currentStudent; // identifing the student on the image
 let guesses; // variabel for showing number of guesses
 let maxRounds; // variabel for identifing the games max amount of rounds
@@ -46,25 +41,14 @@ const shuffleArray = (array) => {
 
 const shuffledStudents = [...students]; // clone `students` array
 shuffleArray(shuffledStudents); // shuffle the `shuffledStudents` array
-console.log("Students after proper shuffling:", shuffledStudents);
 
-// clone of the list off students
-let allStudents = shuffledStudents;
-console.log(allStudents);
+let allStudents = shuffledStudents; // clone of the list off students
+let tenStudents = shuffledStudents.slice(0, 10); // list of 10 students
+let twentyStudents = shuffledStudents.slice(0, 20); // list of 20 students
 
-//list of 10 students
-let tenStudents = shuffledStudents.slice(0, 10);
-console.log(tenStudents);
-
-//list of 20 students
-let twentyStudents = shuffledStudents.slice(0, 20);
-console.log(twentyStudents);
-
-let currentIndex = 0;
 //function to get three random options for names + correct name on shuffled places
 const gameRound = () => {
   currentStudent = shuffledStudents[currentIndex]; //the correnct name for image
-  console.log("currentStudent: ", currentStudent.name);
 
   const allAnswers = buttonOptions(currentStudent, shuffledStudents);
   console.log({ allAnswers });
@@ -85,12 +69,9 @@ const gameRound = () => {
   }
 };
 
+// function for three incorrect button options
 const buttonOptions = (currentStudent, shuffledStudents) => {
-  console.log({ currentStudent });
-  console.log({ shuffledStudents });
-
   const randomNumbers = Math.random() * (students.length - 3);
-  console.log("randomNumbers: ", randomNumbers);
 
   const incorrectOptions = shuffledStudents
     .filter((student) => student.name !== currentStudent.name)
@@ -100,13 +81,7 @@ const buttonOptions = (currentStudent, shuffledStudents) => {
   return incorrectOptions.concat(currentStudent.name);
 };
 
-// // Function for a new round off the game
-// const newRound = () => {
-//   shuffleArray(shuffledStudents);
-//   gameRound();
-// };
-
-// Function for resetting the game state for a new round
+// function for resetting the game for new round
 const restartGame = () => {
   shuffleArray(shuffledStudents);
   guesses = 0;
@@ -121,18 +96,18 @@ const restartGame = () => {
   unhideElement(firstPageEl);
 };
 
-// Function for updating DOM with guesses made
+// function for updating DOM with guesses made
 const updateGuesses = (guess) => {
   guessesEl.innerText = guess === 1 ? `${guess} guess` : `${guess} guesses`;
 };
 
-//function to show in markup the amount of right vs wrong guesses per round
+// function to show in markup the amount of right vs wrong guesses per round
 function updateCounters() {
   rightGuessesEl.innerText = `Right Guesses: ${rightGuesses}`;
   wrongGuessesEl.innerText = `Wrong Guesses: ${wrongGuesses}`;
 }
 
-//function using ternary operator to display GIF according to how good the user are at guessing the names
+// function to display results in the end off the game
 function displayResults() {
   const resultOutput =
     rightGuesses >= wrongGuesses ? "images/winner.gif" : "images/loser.gif";
@@ -144,16 +119,15 @@ function displayResults() {
     )
     .join("");
 
-  resultsEl.innerHTML = `<figure><img class="img-fluid" src=${resultOutput}></figure>
-  <ul class="result-list"> Wrong guessed students: ${wrongGuessesDisplay}
-  </ul>
-  <p>You had ${rightGuesses} right guesses and ${wrongGuesses} wrong guesses</p>
+  resultsEl.innerHTML = `<figure><img id="result-gif" class="img-fluid" src=${resultOutput}></figure>
+  <h3>Wrong guessed students:</h3>
+  <ul class="result-list">${wrongGuessesDisplay}</ul>
+  <p id="result-p">🟢 You had ${rightGuesses} right guesses and ${wrongGuesses} wrong guesses 🔴</p>
   <button id="restartButton">Restart new game!</button>
-  
   `;
 }
 
-// Event listner for how many rounds to output
+// event listner: listening for how many rounds the user want´s to play
 optionsButtonsEl.addEventListener("click", (e) => {
   e.preventDefault();
   e.stopPropagation();
@@ -164,18 +138,14 @@ optionsButtonsEl.addEventListener("click", (e) => {
     if (e.target.value === "10") {
       allStudents = tenStudents;
       maxRounds = 10;
-      console.log("selected 10 students: ", tenStudents);
     } else if (e.target.value === "20") {
       allStudents = twentyStudents;
       maxRounds = 20;
-      console.log("selected 20 students: ", twentyStudents);
     } else if (e.target.value === "all") {
       allStudents = shuffledStudents;
       maxRounds = allStudents.length;
-      console.log("Selected 41 students:", allStudents);
     }
 
-    // maxRounds = allStudents.length;
     rounds = 0;
   }
 
@@ -187,23 +157,19 @@ optionsButtonsEl.addEventListener("click", (e) => {
   hideElement(firstPageEl);
 });
 
-// event listner for the four option buttons
+// event listner: listening for the four option buttons off names
 allGuessingButtonsEl.addEventListener("click", (e) => {
   e.preventDefault();
   e.stopPropagation();
-
-  // console.log("rounds:", rounds, "maxRounds:", maxRounds);
 
   if (e.target.tagName === "BUTTON") {
     const guessedName = e.target.innerHTML;
 
     if (guessedName === currentStudent.name) {
-      // console.log("correct");
       e.target.style.backgroundColor = "green";
       rightGuesses++;
       rightGuessesList.push(currentStudent);
     } else if (guessedName !== currentStudent.name) {
-      // console.log("wrong");
       e.target.style.backgroundColor = "red";
       wrongGuesses++;
       wrongGuessesList.push({
@@ -211,19 +177,13 @@ allGuessingButtonsEl.addEventListener("click", (e) => {
         currentStudent: currentStudent,
       });
     }
-    console.log(`correct answear: ${currentStudent.name}`);
-
-    console.log("rounds:", rounds, "maxRounds:", maxRounds);
 
     guesses++;
     updateGuesses(guesses);
-
     updateCounters();
     rounds++;
 
     if (rounds < maxRounds) {
-      console.log("maxRounds: ", maxRounds);
-      console.log("rounds: ", rounds);
       setTimeout(() => {
         e.target.style.backgroundColor = "";
         gameRound();
@@ -237,31 +197,9 @@ allGuessingButtonsEl.addEventListener("click", (e) => {
   }
 });
 
-// event listner for the reset game button
+// event listner: listening for the user resetting the game
 resultsEl.addEventListener("click", (e) => {
   if (e.target.tagName === "BUTTON") {
-    console.log("button clicked");
-
     restartGame();
   }
 });
-
-/*
-
-    //   if (rounds >= maxRounds) {
-    //     displayResults();
-    //     e.target.style.backgroundColor = "";
-    //     hideElement(secondPageEl);
-    //     guesses = 0;
-    //   } else {
-    //     setTimeout(() => {
-    //       e.target.style.backgroundColor = "";
-    //       gameRound();
-    //     }, 1000);
-    //   }
-
-
-
-<button id="restartButton">Restart new game!</button>
-
-*/
